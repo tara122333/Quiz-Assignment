991eb5637434fd5d2cffad0c6f2e6c1e4e0e3791
TARA CHAND KUMAWAT <tarachand122333@gmail.com>
2023-04-13 23:41:06 +0530

    Navbar added Home link

--- src/components/Navbar/QuestionNavbar.jsx
+++ src/components/Navbar/QuestionNavbar.jsx
@@ -1,5 +1,6 @@
 import React, {  useState } from 'react';
 import QuizQuestionPopUpModel from '../QuizEditing/QuizQuestionPopUp';
+import { Link } from 'react-router-dom';
 
 const QuestionNavbar = (props) => {
     const [quizQuestionOpen, setQuizQuestionOpen] = useState(false);
@@ -8,16 +9,20 @@ const QuestionNavbar = (props) => {
     <>
         <QuizQuestionPopUpModel isOpen={quizQuestionOpen} setIsOpen={setQuizQuestionOpen} />
         <nav className='flex justify-evenly items-center py-1 bg-green-600'>
-            <div className='h-12 flex justify-center items-center'>
-                <span className='text-xl font-bold text-white'>
+            <div className='h-12 flex justify-evenly items-center gap-1 '>
+                <Link to={"/"}>
+                    <h1 className='px-4 py-1 bg-blue-500 rounded-lg cursor-pointer text-white hover:bg-blue-700 '>
+                        Home
+                    </h1>
+                </Link>
+                
+            </div>
+            <div>
+            <span className='text-xl font-bold text-white'>
                     {
                         props.quizName
                     }
                 </span>
-            </div>
-            <div>
-                <h1 className='font-bold text-lg text-white
-                '>Tara's Quiz Application</h1>
             </div>
             <div className='bg-blue-500 hover:bg-blue-700 px-4 text-white rounded-lg py-1 flex justify-center items-center md:px-6 lg:px-8 lg:cursor-pointer lg:rounded-sm' onClick={quizQuestionOpenModel}>
                 <span className='font-semibold text-lg'>Add Question</span>
